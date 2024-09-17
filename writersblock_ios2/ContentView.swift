8cc15e6dbd7b3db96df31f62f3e62bc1461f6bd7
addisoneverett <addisonreverett@gmail.com>
2024-09-17 13:56:45 -0600

    Initial Commit

---
+++ writersblock_ios2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  writersblock_ios2
+//
+//  Created by Addison Everett on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ writersblock_ios2/writersblock_ios2App.swift
@@ -0,0 +1,17 @@
+//
+//  writersblock_ios2App.swift
+//  writersblock_ios2
+//
+//  Created by Addison Everett on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct writersblock_ios2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
